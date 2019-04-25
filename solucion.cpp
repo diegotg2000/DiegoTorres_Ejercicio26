634cd4080863585a73dda6e08a626f1a8d22c767
Diego Torres <diegotg2000@hotmail.com>
2019-04-25 12:18:51 +0000

    Se añaden las soluciones finales

--- solucion.cpp
+++ solucion.cpp
@@ -8,13 +8,15 @@ using namespace std;
 
 void explicit_Euler(float h);
 void rk4(float h);
+void lf(float h);
 float w=1.0;
 float Y=1.0;
-float T=30.0;
+float T=10000.0;
 
 int main(){
-    explicit_Euler(T/50000); 
-    rk4(T/50000);
+    explicit_Euler(0.5);
+    rk4(0.5);
+    lf(0.5);
     return 0;
 }
 
@@ -28,13 +30,13 @@ void explicit_Euler(float h){
     int N=T/h;
     yn=Y;
     vn=0;
-    outfile<<0<<' '<<yn<<endl;
+    outfile<<0<<' '<<yn<<' '<<vn<<endl;
     for(int i=1;i<=N;i++){
         yn1=yn+h*vn;
         vn1=vn-h*w*w*yn;
         yn=yn1;
         vn=vn1;
-        outfile<<i*h<<' '<<yn<<endl;        
+        outfile<<i*h<<' '<<yn<<' '<<vn<<endl;        
     }
     
     outfile.close();
@@ -51,7 +53,7 @@ void rk4(float h){
     float k21,k22,k23,k24;
     yn=Y;
     vn=0;
-    outfile<<0<<' '<<yn<<endl;
+    outfile<<0<<' '<<yn<<' '<<vn<<endl;
     for(int i=1;i<=N;i++){
         k11=h*(vn);
         k21=h*(-w*w*yn);
@@ -69,7 +71,30 @@ void rk4(float h){
         vn1=vn+(k21+2*k22+2*k23+k24)/6;
         yn=yn1;
         vn=vn1;
-        outfile<<i*h<<' '<<yn<<endl;        
+        outfile<<i*h<<' '<<yn<<' '<<vn<<endl;        
+    }
+    
+    outfile.close();
+}
+void lf(float h){
+    ofstream outfile;
+
+    outfile.open("lf.txt");
+
+    float yn, yn1;
+    float vn,vn1;
+    float vn12;
+    int N=T/h;
+    yn=Y;
+    vn=0;
+    outfile<<0<<' '<<yn<<' '<<vn<<endl;
+    for(int i=1;i<=N;i++){
+        vn12=vn + h/2*(-w*w*yn);
+        yn1=yn+vn12*h;
+        vn1=vn12 + h/2*(-w*w*yn1);
+        yn=yn1;
+        vn=vn1;
+        outfile<<i*h<<' '<<yn<<' '<<vn<<endl;        
     }
     
     outfile.close();
